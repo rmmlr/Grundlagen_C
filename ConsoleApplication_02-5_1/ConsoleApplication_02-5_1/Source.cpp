225a3837a4979fe50320719ead69bb566c95bd5f
Elias Ruemmler <elias.ruemmler@icloud.com>
2018-03-10 12:31:42 +0100

    Aufgabe 02-5.1 ergänzt

---
+++ ConsoleApplication_02-5_1/ConsoleApplication_02-5_1/Source.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+
+using namespace std;
+
+
+void main(void)
+{
+	// Wenn Programm nicht startet (keine Konsolenausgabe) bitte folgende Zeile
+	// auskommentieren oder Anwendung mit Administratorrechten neu starten.
+	locale::global(locale("German_germany"));
+
+
+	//Variablendeklaration
+
+
+	//Work
+	cout << "Praktikumsaufgabe: Nr.: 02-5.1" << endl;
+	cout << endl;
+
+
+	cout << "Aufgabe 1. a)" << endl;
+	cout << endl;
+	for (int i = 1; i <= 100; i++)
+		cout << i << "; ";
+	cout << endl;
+	cout << endl;
+
+	cout << "Aufgabe 1. b)" << endl;
+	cout << endl;
+	for (int i = 100; i > 0; i--)
+		cout << i << "; ";
+	cout << endl;
+	cout << endl;
+
+
+	cout << "Aufgabe 2." << endl;
+	cout << endl;
+
+	int value = 1000;
+
+	for (int i = 1; i <= 100; i++)
+		value -= i;
+
+	cout << "Die Endsumme beträgt: " << value << endl;
+	cout << endl;
+
+	cout << endl;
+	system("pause");
+}
