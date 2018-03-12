ca6cae7bb059fb62e98fde358c30a4102127a644
Elias Ruemmler <git@rc-art.de>
2018-03-12 14:33:02 +0100

    Add c++ console template

--- ConsoleApplication_eruemmle/ConsoleApplication_eruemmle/Source.cpp
+++ ConsoleApplication_eruemmle/ConsoleApplication_eruemmle/Source.cpp
@@ -1,33 +1,40 @@
+/*
+Grundlagen Programmierung C
+
+Autor:	Ruemmler, Elias
+Datum:	12.03.2018
+Repo:	https://github.com/rmmlr/Grundlagen_C
+
+Praktikumsaufgabe: Nr.: 0x-x.x
+*/
+
 #include <iostream>
 
 using namespace std;
 
 void main(void)
 {
-	// Wenn Programm nicht startet (keine Konsolenausgabe) bitte folgende Zeile auskommentieren
+	// Wenn Programm nicht startet (keine Konsolenausgabe) bitte folgende Zeile
+	// auskommentieren oder Anwendung mit Administratorrechten neu starten.
 	locale::global(locale("German_germany"));
 
-	cout << "Test-Projekt:" << endl << endl;
 
-	bool b = false;
-	int i = 0;
-	long l = 45110465;
-	float f = 0;
-	double d = 0;
-	
-	cout << "Sizeof Datentyp" << endl;
-
-	cout << "Bool:\t\t" << sizeof(b) << endl;
-	cout << "Integer:\t" << sizeof(i) << endl;
-	cout << "Long:\t\t" << sizeof(l) << endl;
-	cout << "Float:\t\t" << sizeof(f) << endl;
-	cout << "Double:\t\t" << sizeof(d) << endl;
+	//Variablendeklaration
 
 
 
 
+	//Ausgabekopf
+	cout << "Praktikumsaufgabe: Nr.: 0x-x.x" << endl << endl;
 
-	cout << endl;
-	cout << endl;
+
+
+	//Work
+
+
+
+
+	//Programmende
+	cout << endl << endl;
 	system("pause");
 }
---
+++ ProjectTemplate/Readme.md
@@ -0,0 +1,10 @@
+# VisualStudio Project Templates
+
+## Inhalt
+
+* ConsoleApplication_GrundlagenC.zip
+  Projektvorlage zum Erstellen einer Win32 Konsolenanwendung. In der Quelldatei Source.cpp ist die Main-Funktion bereits angelegt.
+
+
+## Installation
+Zip-Datei in das lokale Template-Verzeichnis (C:\Users\<UserName>\Documents\Visual Studio 2015\Templates\ProjectTemplates\Visual C++ Project) kopieren.
