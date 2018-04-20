80d82c3d80ebb3ccfa6a3105928c30b51ff0474f
Elias Ruemmler <elias.ruemmler@icloud.com>
2018-04-20 18:59:17 +0200

    rechnung wiederholen

--- Komplex_02/ConsoleApplication_02-3_1/ConsoleApplication_02-3_1/Source.cpp
+++ Komplex_02/ConsoleApplication_02-3_1/ConsoleApplication_02-3_1/Source.cpp
@@ -21,53 +21,62 @@ void main(void)
 	//Ergebnis
 	float result = 0;
 
+	//Anwort der Wiederholungsfrage
+	char ans;
+
 	//Work
 	cout << "Praktikumsaufgabe: Nr.: 02-3.1" << endl;
-	cout << endl;
 
-	cout << "Bitte ersten Operanden eingeben: ";
-	cin >> a;
-	cout << endl;
-
-	cout << "Bitte Rechenfunktion (+, -, *, /) eingeben: ";
-	cin >> function;
-	cout << endl;
-
-	cout << "Bitte zweiten Operanden eingeben: ";
-	cin >> b;
-	cout << endl;
-	cout << endl;
-
-	if (function == '/' && b == 0)
-		cout << "Division durch 0 nicht möglich!" << endl;
-	else
+	do
 	{
-		switch (function)
-		{
-		case '+':
-			result = a + b;
-			break;
-		case '-':
-			result = a - b;
-			break;
-		case '*':
-		case 'x':
-			result = a * b;
-			break;
-		case '/':
-			result = a / b;
-			break;
-		default:
-			cout << "Die eingegebene Rechenfunktion '" << function << "' wird nicht unterstützt." << endl;
-			cout << endl;
-			system("pause");
-			return;
-		}
-
-		cout << "Ergebnis:" << endl;
-		cout << a << " " << function << " " << b << " = " << result << endl;
-	}
+		cout << endl;
 	
+		cout << "Bitte ersten Operanden eingeben: ";
+		cin >> a;
+		cout << endl;
+
+		cout << "Bitte Rechenfunktion (+, -, *, /) eingeben: ";
+		cin >> function;
+		cout << endl;
+
+		cout << "Bitte zweiten Operanden eingeben: ";
+		cin >> b;
+		cout << endl;
+		cout << endl;
+
+		if (function == '/' && b == 0)
+			cout << "Division durch 0 nicht möglich!" << endl;
+		else
+		{
+			switch (function)
+			{
+			case '+':
+				result = a + b;
+				break;
+			case '-':
+				result = a - b;
+				break;
+			case '*':
+			case 'x':
+				result = a * b;
+				break;
+			case '/':
+				result = a / b;
+				break;
+			default:
+				cout << "Die eingegebene Rechenfunktion '" << function << "' wird nicht unterstützt." << endl;
+				cout << endl;
+				system("pause");
+				return;
+			}
+
+			cout << "Ergebnis:" << endl;
+			cout << a << " " << function << " " << b << " = " << result << endl;
+
+			cout << "Soll noch eine Aufgabe gerechnet werden? (J/N)" << endl;
+			cin >> ans;
+		}
+	} while ((ans == 'J' || ans == 'j'));
 
 	cout << endl;
 	system("pause");
