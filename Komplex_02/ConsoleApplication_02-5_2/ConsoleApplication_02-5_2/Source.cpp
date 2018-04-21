ef518cd3aaf8aa32f4a25eaf5150e2ff61fda3d6
Elias Ruemmler <elias.ruemmler@icloud.com>
2018-04-21 12:55:01 +0200

    cout: scientific -> fixed

--- Komplex_02/ConsoleApplication_02-5_2/ConsoleApplication_02-5_2/Source.cpp
+++ Komplex_02/ConsoleApplication_02-5_2/ConsoleApplication_02-5_2/Source.cpp
@@ -8,7 +8,13 @@ void main(void)
 	// Wenn Programm nicht startet (keine Konsolenausgabe) bitte folgende Zeile
 	// auskommentieren oder Anwendung mit Administratorrechten neu starten.
 	locale::global(locale("German_germany"));
+	
+	// Ausgabe der Dezimalstellen für Double verhindern (es werden 0 Nachkommastellen angezeigt)
+	std::cout.precision(0); 
 
+	// Ausgabe von Zahlen auf "Fixed" stellen, große Zahlen werden ansonsten im Wissenschaftlichen Format ausgegeben
+	// Einstellung wirkt sich auf alle folgenden Ausgaben per "cout" aus
+	cout << fixed;
 
 	//Variablendeklaration
 	//Eingabewert (1 - 30)
@@ -27,13 +33,14 @@ void main(void)
 	if (inputValue < 1 || inputValue > 30)
 		cout << "Eingegebener Wert '" << inputValue << "' nicht im zulässigen Bereich" << endl;
 	cout << endl;
-	cout << endl;
 
 	long double result = 1;
 	for (int i = 1; i <= inputValue; i++)
-		result *= i;
+		result = result * i; //result *= i;
 
 	cout << "Die Fakultät für " << inputValue << " beträgt: " << result << endl;;
+	cout << endl;
+	cout << endl;
 
 	cout << "Aufgabe 2." << endl;
 	cout << endl;
